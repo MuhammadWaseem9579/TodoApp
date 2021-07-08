7baecd6ac9605bca28234e32d3644f6aceffef6f
Muhammad Waseem <muhammadwaseem9579@gmail.com>
2021-07-08 14:49:22 +0500

    last commit after refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,7 @@
 class TodosController < ApplicationController
 	
-<<<<<<< HEAD
 	before_action :set_todo, only: [:edit,:update,:destroy,:show]
 
-=======
->>>>>>> 1a36d2a1673677fdbaf1f78e582d3a81e5f80170
 	def new
 		@todo = Todo.new
 	end
@@ -45,11 +42,6 @@ class TodosController < ApplicationController
 
 
 	def destroy
-<<<<<<< HEAD
-		
-=======
-		@todo = Todo.find(params[:id])
->>>>>>> 1a36d2a1673677fdbaf1f78e582d3a81e5f80170
 		if @todo.destroy
 			redirect_to todos_path
 		else
